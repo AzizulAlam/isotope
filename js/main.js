7c3bb55ec65e0a0201bb5107cc258dd0cfb00090
AzizulAlam <azizul17bd@gmail.com>
2017-05-12 18:52:50 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,33 @@
+(function($) {
+    "use restrict";
+    
+   jQuery(document).ready(function($){
+    
+//    $(".video-play-btn").magnificPopup({
+//       type:'video', 
+//    });
+    
+        $(".project-title li").on('click', function(){
+            
+            $(".project-title li").removeClass("active");
+            $(this).addClass("active");
+            
+            var selector = $(this).attr('data-filter');
+            $(".project-list").isotope({
+                filter: selector
+            });
+        });
+       
+       
+       
+   }); 
+    
+    jQuery(window).load(function(){
+    
+        jQuery(".project-list").isotope();
+        
+    });
+    
+}(jQuery));
+
+ 
